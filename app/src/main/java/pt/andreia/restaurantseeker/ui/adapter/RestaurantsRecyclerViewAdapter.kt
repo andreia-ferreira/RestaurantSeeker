33ce80c770b2d970b3370359076ee001db3c9da7
veronica <andreia.mp.ferreira@gmail.com>
2020-08-08 16:40:12 +0100

    +add recyclerView restaurants

---
+++ app/src/main/java/pt/andreia/restaurantseeker/ui/adapter/RestaurantsRecyclerViewAdapter.kt
@@ -0,0 +1,51 @@
+package pt.andreia.restaurantseeker.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import pt.andreia.restaurantseeker.R
+import pt.andreia.restaurantseeker.databinding.ItemRestaurantBinding
+import pt.andreia.restaurantseeker.model.Restaurant
+
+class RestaurantsRecyclerViewAdapter: ListAdapter<Restaurant, RecyclerView.ViewHolder>(RestaurantsDiffCallback()) {
+
+    private lateinit var binding: ItemRestaurantBinding
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        binding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_restaurant,
+            parent,
+            false)
+        return RestaurantViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val restaurant = getItem(position)
+        (holder as RestaurantViewHolder).bind(restaurant)
+    }
+
+    inner class RestaurantViewHolder(private val binding: ItemRestaurantBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(restaurant: Restaurant) {
+            binding.restaurant = restaurant
+        }
+
+    }
+
+}
+
+private class RestaurantsDiffCallback: DiffUtil.ItemCallback<Restaurant>() {
+    override fun areItemsTheSame(oldItem: Restaurant, newItem: Restaurant): Boolean {
+        return oldItem.name == newItem.name
+    }
+
+    override fun areContentsTheSame(oldItem: Restaurant, newItem: Restaurant): Boolean {
+        return oldItem == newItem
+    }
+
+}
--- app/src/main/java/pt/andreia/restaurantseeker/ui/main/MainFragment.kt
+++ app/src/main/java/pt/andreia/restaurantseeker/ui/main/MainFragment.kt
@@ -12,23 +12,28 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import pt.andreia.restaurantseeker.R
 import pt.andreia.restaurantseeker.databinding.MainFragmentBinding
+import pt.andreia.restaurantseeker.ui.adapter.RestaurantsRecyclerViewAdapter
 
 class MainFragment : Fragment() {
 
     private val viewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
+    private val adapter: RestaurantsRecyclerViewAdapter by lazy { RestaurantsRecyclerViewAdapter() }
     private lateinit var binding: MainFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+
+        binding.recyclerViewRestaurants.adapter = adapter
         initObservers()
+
         return binding.root
     }
 
     private fun initObservers() {
         viewModel.listRestaurants.observe(viewLifecycleOwner, Observer {
             if (it != null) {
-                Log.d("xpto", it.toString())
+                adapter.submitList(it)
             }
         })
     }
