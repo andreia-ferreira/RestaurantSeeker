2cc82cd16e20dacc9ded6ababdb25965df02ae92
veronica <andreia.mp.ferreira@gmail.com>
2020-08-08 12:00:44 +0100

    init project

---
+++ app/src/main/java/pt/andreia/restaurantseeker/MainActivity.kt
@@ -0,0 +1,18 @@
+package pt.andreia.restaurantseeker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pt.andreia.restaurantseeker.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/pt/andreia/restaurantseeker/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package pt.andreia.restaurantseeker.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
