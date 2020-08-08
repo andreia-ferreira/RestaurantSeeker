bacf41077cc064d6b89691d67eb021465edf6c01
veronica <andreia.mp.ferreira@gmail.com>
2020-08-08 12:16:35 +0100

    update dependencies; +add assets

--- app/src/main/java/pt/andreia/restaurantseeker/ui/main/MainFragment.kt
+++ app/src/main/java/pt/andreia/restaurantseeker/ui/main/MainFragment.kt
@@ -6,25 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import pt.andreia.restaurantseeker.R
+import pt.andreia.restaurantseeker.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
-    private lateinit var viewModel: MainViewModel
+    private val viewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
+    private lateinit var binding: MainFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    companion object {
+        fun newInstance() = MainFragment()
     }
 
 }
--- app/src/main/java/pt/andreia/restaurantseeker/ui/main/MainViewModel.kt
+++ app/src/main/java/pt/andreia/restaurantseeker/ui/main/MainViewModel.kt
@@ -3,5 +3,5 @@ package pt.andreia.restaurantseeker.ui.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
