ac893356578074e2c9e9ba47434f44c43632400c
veronica <andreia.mp.ferreira@gmail.com>
2020-08-11 21:52:06 +0100

    update database test

---
+++ app/src/androidTest/java/pt/andreia/restaurantseeker/DatabaseTest.kt
@@ -0,0 +1,49 @@
+package pt.andreia.restaurantseeker
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import pt.andreia.restaurantseeker.database.RestaurantDao
+import pt.andreia.restaurantseeker.database.RestaurantDatabase
+import pt.andreia.restaurantseeker.model.dto.FavoriteRestaurantEntity
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class DatabaseTest {
+
+    private lateinit var restaurantDao: RestaurantDao
+    private lateinit var database: RestaurantDatabase
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        database = Room.inMemoryDatabaseBuilder(context, RestaurantDatabase::class.java).build()
+        restaurantDao = database.restaurantDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        database.close()
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun addFavorites() = runBlocking {
+        val favorite = FavoriteRestaurantEntity("test")
+        restaurantDao.saveToFavorites(favorite)
+
+        val databaseRestaurant = restaurantDao.getFavorites()
+
+        assertEquals("test", databaseRestaurant[0].name)
+    }
+
+}
